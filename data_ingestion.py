64be06836ffaf94d299d4b9d3d99d4d3d303962a
Ramses <ramsesloaces@gmail.com>
2025-12-16 11:47:23 -0500

    Added Generation of table summaries

--- data_ingestion.py
+++ data_ingestion.py
@@ -2,15 +2,16 @@ import langchain
 from langchain_text_splitters import Language, RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import UnstructuredMarkdownLoader
 
-
+from LLMRAG import client
 
 # === System Utils === 
 import os 
 import pathlib
 import glob
 
+import json
+import hashlib
 
-import nltk
 
 """
 import ssl
@@ -28,6 +29,11 @@ nltk.download('punkt_tab')"""
 pattern = "./Data/*/text/*"
 files = glob.glob(pattern, recursive=True)
 
+files = [file for file in files if ".json" not in file]  # remove json files from initial search 
+
+
+def hash_table(table_html: str) -> str:
+    return hashlib.sha256(table_html.encode("utf-8")).hexdigest()
 
 
 def document_chunking(file: str) -> None: 
@@ -60,8 +66,67 @@ def document_chunking(file: str) -> None:
         else:
             text[item] = doc[item]
 
+    # === check if there are any tables 
+    if len(raw_tables) > 0:     
 
+        # === check if there is document table summary data is already stored === 
+
+        table_out = file.split('/')[-1].split('.')[0] + "_table_summaries.json" # table file name
+        cur_dir = pathlib.Path(file).parent
+        table_out_path = os.path.join(cur_dir, table_out)
+
+            # Load Tables or Create new tables 
+        if os.path.exists(table_out_path):
+            with open(table_out_path, "r") as f:
+                table_summaries = json.load(f)
+        else:
+            table_summaries = {}
+
+        for key, table_doc in raw_tables.items():
+
+            table_html = table_doc.metadata.get("text_as_html")
+            table_hash = hash_table(table_html)
+
+            # Skip if already summarized
+            if table_hash in table_summaries:
+                continue
+            
+            # Create table summary
+            response = client.models.generate_content(
+                        model="gemini-2.0-flash-001",
+                        contents={"text": table_html},
+                        config={
+                            "max_output_tokens": 1000,
+                            "temperature": 0,
+                            "top_p": 0.95,
+                            "top_k": 20,
+                            "system_instruction": (
+                                "You are an expert assistant that summarizes tables accurately, "
+                                "preserving all key information."),
+                            },
+                        )
+            # store table summary 
+            table_summaries[table_hash] = { "summary": response.text,
+                                            "element_id": table_doc.metadata.get("element_id"),
+                                            "source": table_doc.metadata.get("source"),
+                                            "model": "gemini-2.0-flash-001",
+                                            }
+            
+
+        with open(table_out_path, "w") as f:
+            json.dump(table_summaries, f, indent=2)
     
+
+
+
+
+
+
+        
+
+
+
+
 """    markdown_splitter = RecursiveCharacterTextSplitter.from_language(
     language=Language.MARKDOWN,
     chunk_size=512,        # Set your desired chunk size
