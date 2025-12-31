95d1968acfaf680d93e43bef4c073cee122cbbc9
Ramses <ramsesloaces@gmail.com>
2025-12-30 19:23:34 -0500

    Added gemini embedding model using langchain and added chromadb persistent vectore store.

--- LLMRAG.py
+++ LLMRAG.py
@@ -5,6 +5,12 @@
 
 # === Google GenAI ===
 from google import genai
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain.chat_models import init_chat_model
+from langchain_chroma import Chroma
+
+import chromadb
+from dotenv import load_dotenv
 
 # === Frontend ===
 # import gradio as gr 
@@ -38,4 +44,36 @@ client = genai.Client(api_key=api_key)
 
 print(response.text) """
 
-    
+
+load_dotenv()
+
+api_key = os.environ["GOOGLE_GENAI_API_KEY"]
+
+model = model = init_chat_model("google_genai:gemini-2.5-flash-lite", api_key=api_key)
+
+# === embeddings === 
+
+# TODO ==== Update to google doc method of loading embeddings === 
+# to build using google doc a class object must be built 
+# the langchain object below is a pydantic class object which contains client initialization and 
+# embedding creation methods ""
+
+# initialized client 
+embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001", api_key=api_key)
+
+# === Setup Vectore store and Persistent Storage  ===
+
+if not os.path.exists("./chroma_db"):
+    chroma_client = chromadb.PersistentClient(path="./chroma_db")
+    chroma_client.create_collection(
+    name="example_collection",
+    embedding_function=embeddings,
+    configuration={
+        "hnsw": {
+            "space": "cosine",
+            "ef_construction": 200
+        }
+    }
+)
+else: 
+    chroma_client = chromadb.PersistentClient(path="./chroma_db")
